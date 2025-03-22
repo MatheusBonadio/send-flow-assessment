917e090deeb4797216f8f0a16cfb56d9464487a0
Matheus Bonadio <matheus_lima_bonadio_xd@hotmail.com>
2025-03-22 01:45:45 -0300

    feat: configured firebase function to schedule messages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,34 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onSchedule } from 'firebase-functions/v2/scheduler';
+import * as admin from 'firebase-admin';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const updateMessageStatus = onSchedule('every 1 minutes', async (event) => {
+  const firestore = admin.firestore();
+  const now = new Date();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  const usersRef = firestore.collection('users');
+
+  try {
+    const usersSnapshot = await usersRef.get();
+
+    for (const userDoc of usersSnapshot.docs) {
+      const messagesRef = userDoc.ref.collection('messages');
+
+      const query = messagesRef
+        .where('status', '==', 'scheduled')
+        .where('scheduledAt', '<=', now);
+
+      const messagesSnapshot = await query.get();
+
+      for (const messageDoc of messagesSnapshot.docs) {
+        await messageDoc.ref.update({ status: 'sent' });
+        console.log(`Mensagem ${messageDoc.id} do usuário ${userDoc.id} atualizada para 'sent'`);
+      }
+
+      console.log(`Total de mensagens atualizadas para o usuário ${userDoc.id}: ${messagesSnapshot.size}`);
+    }
+  } catch (error) {
+    console.error('Erro ao atualizar mensagens:', error);
+  }
+});
