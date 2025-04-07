3ac0ffb70e4ce2d5f3ece0dd0bdee5e01663d29c
Matheus Bonadio <matheus_lima_bonadio_xd@hotmail.com>
2025-04-07 08:14:41 -0300

    refactor: firebase functions optimization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,27 +7,19 @@ export const updateMessageStatus = onSchedule('every 1 minutes', async (event) =
   const firestore = admin.firestore();
   const now = new Date();
 
-  const usersRef = firestore.collection('users');
+  const messagesRef = firestore.collectionGroup('messages');
 
   try {
-    const usersSnapshot = await usersRef.get();
-
-    for (const userDoc of usersSnapshot.docs) {
-      const messagesRef = userDoc.ref.collection('messages');
-
-      const query = messagesRef
-        .where('status', '==', 'scheduled')
-        .where('scheduledAt', '<=', now);
+    const query = messagesRef
+      .where('status', '==', 'scheduled')
+      .where('scheduledAt', '<=', now);
 
       const messagesSnapshot = await query.get();
 
       for (const messageDoc of messagesSnapshot.docs) {
         await messageDoc.ref.update({ status: 'sent' });
-        console.log(`Mensagem ${messageDoc.id} do usuário ${userDoc.id} atualizada para 'sent'`);
+        console.log(`Mensagem ${messageDoc.id} atualizada para 'sent'`);
       }
-
-      console.log(`Total de mensagens atualizadas para o usuário ${userDoc.id}: ${messagesSnapshot.size}`);
-    }
   } catch (error) {
     console.error('Erro ao atualizar mensagens:', error);
   }
