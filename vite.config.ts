bc27cb63b6410ff96b400bd5dfff417cc715a398
Matheus Bonadio <matheus_lima_bonadio_xd@hotmail.com>
2025-04-07 14:12:22 -0300

    refactor: reconfigured tailwind with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), tailwindcss(), viteTsconfigPaths()],
   server: {
     open: true,
   },
