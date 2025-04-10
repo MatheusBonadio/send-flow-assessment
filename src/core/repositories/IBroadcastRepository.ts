9778a35db8df0b094906adb25e92ccba6ccf06bd
MatheusBonadio <matheus_lima_bonadio_xd@hotmail.com>
2025-04-10 10:54:30 -0300

    refactor: old entities and repositories deleted

--- src/core/repositories/IBroadcastRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Broadcast } from "@/core/entities/broadcast";
-
-export interface IBroadcastRepository {
-    create(broadcast: Broadcast): Promise<Broadcast>;
-    getById(id: string): Promise<Broadcast | null>;
-    getAll(onDataChanged: (broadcasts: Broadcast[]) => void): void;
-    delete(id: string): Promise<boolean>;
-    getCount(onCountChanged: (count: number) => void): void;
-}
--- src/core/repositories/IUserRepository.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "@/core/entities/user";
-
-export interface IUserRepository {
-    create(broadcast: User): Promise<User>;
-}
